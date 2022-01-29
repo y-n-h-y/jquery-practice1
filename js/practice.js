9955da93168c778b4d062f0e1355d1378b6e4ae9
吉野弘之 <yoshinohiroyuki@yoshinohiroyuki-no-MacBook-Pro.local>
2022-01-29 21:58:00 +0900

    animation

--- js/practice.js
+++ js/practice.js
@@ -1,3 +1,24 @@
-$(document).ready(function () {
-  $('body').html('<h1>hello world</h1>');
+// $(document).ready(function () {
+//   $('body').html('<h1>hello world</h1>');
+// });
+
+// $(function () {
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// $(function () {
+//   $('.box1').slideUp();
+// });
+
+// $(function () {
+//   $('.box1').show();
+//   $('.box1').css({
+//     'background-color': '#0000FF'
+//   });
+// });
+$(function () {
+  $('.box1').hide();
 });
